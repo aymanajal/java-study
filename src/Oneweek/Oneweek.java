36afa7c25976e636489297c482dedfa846d623d8
Yimin Wang <aymanajalaalf@hotmail.com>
2025-03-05 00:08:30 +0800

    1.3浮点数计算

--- src/Oneweek/Oneweek.java
+++ src/Oneweek/Oneweek.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 
 public class Oneweek {
     public static void main(String[] args) {
-        // System.out.println("Hello, World!");
-        try (Scanner in = new Scanner(System.in)) {
-            int a = in.nextInt(); // 变量a
-            int b =a;
-            System.out.println("输出变量b：" + b);
-            System.out.println("100-" + a + "=" + (100-a));
-        }
+        //1.2变量与计算
+        // // System.out.println("Hello, World!");
+        // try (Scanner in = new Scanner(System.in)) {
+        //     System.out.print("请输入一个整数a：");
+        //     int a = in.nextInt(); // 变量a
+        //     int b =a;
+        //     final int amount = 100;  // 声明一个常量amount，值为100
+        //     // int vat_amount = in.nextInt();
+        //     System.out.println("输出变量b：" + b);
+        //     System.out.println(amount + "-" + a + "=" + (amount-a));
+        // }
+        // 1.3 浮点数计算
+        
     }
 }
